6ec6a7e999763f5d6023278a6165deecc8abeddc
yangy <yangy@ufen.cn>
2020-08-23 16:13:48 +0800

    修改

--- src/main/java/com/ilidan/factory/ComputerFactoryImpl.java
+++ src/main/java/com/ilidan/factory/ComputerFactoryImpl.java
@@ -1,9 +1,10 @@
 package com.ilidan.factory;
 
 public class ComputerFactoryImpl implements ComputerFactory {
+    @Override
     public <T extends Computer> T manufactureComputer(Class<T> clazz) {
         try{
-            return (T) clazz.forName(clazz.getName()).newInstance();
+            return (T) Class.forName(clazz.getName()).newInstance();
         }catch (Exception e){
             throw new RuntimeException("创建实例对象失败！");
         }
